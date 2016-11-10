126b0bfaf0a5e4aed3baa84a51755b6284748305
Jeff Moore <jmas1_2000@hotmail.com>
2016-11-10 15:24:47 -0800

    Assignment 8 Animations completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,26 +1,51 @@
+var animatePoints = function() {
+    var points = document.getElementsByClassName('point');
+    var revealPoint = function(index) {
+        points[index].style.opacity = 1;
+        points[index].style.transform = "scaleX(1) translateY(0)";
+        points[index].style.msTransform = "scaleX(1) translateY(0)";
+        points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+    };     
+    for(var i = 0; i < points.length; i++) {
+    revealPoint(i);
+    }
+};
+
+ 
+ /*Code above has been refactored from code below when asked:
+ 
+Refactor the individual style calls of the landing.js script to be a single function named revealPoint that:
+takes a single argument: the index of the points class node element, and
+gets called in a for loop.
+
  var animatePoints = function() {
+ 
      var points = document.getElementsByClassName('point');
+ 
      var revealFirstPoint = function() {
          points[0].style.opacity = 1;
          points[0].style.transform = "scaleX(1) translateY(0)";
          points[0].style.msTransform = "scaleX(1) translateY(0)";
          points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
      };
+ 
      var revealSecondPoint = function() {
          points[1].style.opacity = 1;
          points[1].style.transform = "scaleX(1) translateY(0)";
          points[1].style.msTransform = "scaleX(1) translateY(0)";
          points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
      };
+ 
      var revealThirdPoint = function() {
          points[2].style.opacity = 1;
          points[2].style.transform = "scaleX(1) translateY(0)";
          points[2].style.msTransform = "scaleX(1) translateY(0)";
          points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
      };
+ 
      revealFirstPoint();
      revealSecondPoint();
      revealThirdPoint();
+ 
  };
-
-             
+ */
